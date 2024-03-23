f2933c232b4e9d5da04cbfe33d2c39b3131be02c
egesua <egesu06@gmail.com>
2024-03-23 19:55:29 +0300

    user entity created. also app properties declared.

---
+++ src/main/java/com/egesua/fsbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.egesua.fsbackend.repository;
+
+import com.egesua.fsbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
